567cca459873e65632c516428a583f22bee33230
Android5730 <577300715@qq.com>
2023-08-27 22:10:17 +0800

    数据抓取开源库：更新模块功能

--- DataCapture/src/main/java/com/itaem/datacapture/Utils/SmsUtil.java
+++ DataCapture/src/main/java/com/itaem/datacapture/Utils/SmsUtil.java
@@ -4,7 +4,6 @@ import android.annotation.SuppressLint;
 import android.content.ContentResolver;
 import android.content.Context;
 import android.database.Cursor;
-import android.net.Uri;
 import android.provider.Telephony;
 
 import com.itaem.datacapture.bean.SmsBean;
